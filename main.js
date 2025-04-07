a3967d08a2fafb35f9fbed0230cd155f6863b180
Quang Sanh <buiquangsanh1501@gmail.com>
2025-04-07 22:05:16 +0700

    feat:add middlewares controllers and services

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
-import express from 'express';
-import dotenv from 'dotenv';
-const app = express();
-dotenv.config();
+import express from 'express'
+import dotenv from 'dotenv'
+import router from './src/routes/index.js'
+const app = express()
+dotenv.config()
+// middleware
+app.use(express.json())
+app.use((req, res, next) => {
+    console.log("Now:", new Date().toLocaleString())
+    next()
+})
+// router
+app.use("/api", router)
+app.use((err, req, res, next) => {
+    console.log("Error:", err)
+    return RegExp.status(500).json(
+        {
+            error:err.mesage
 
-app.use(express.json());
+        })
+})
 
-app.route('/')
-    .get((reg, res) => {
-        res.send("ai là người đẹp trai nhất?Sanh là người đẹp trai nhất")
-
-    }
-    )
-const Port = process.env.PORT || 3000
-app.listen(Port, (reg, res) => {
-    
+const Port = process.env.Port || 3000
+app.listen(Port, (req, res) => {
     console.log(`Server run at http://localhost:${Port}`)
-
 })
